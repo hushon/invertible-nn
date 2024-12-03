56a93f872e0c03cdb96b031bc6220908e07f3b09
Hyounguk Shon <hyounguk.shon@gmail.com>
2024-12-03 19:05:05 +0000

    updated

--- invertible_nn/invertible_vit.py
+++ invertible_nn/invertible_vit.py
@@ -1,70 +1,179 @@
 import torch
 from torch import nn
 import invertible_nn
+import math
 
 
-class MLPSubblock(nn.Module):
-    """
-    This creates the function G such that the entire block can be
-    expressed as F(G(X)). Includes pre-LayerNorm.
-    """
-    def __init__(
-        self,
-        dim,
-        mlp_ratio=4
-    ):
+class Adapter(nn.Module):
+    def __init__(self,
+                 n_embed,
+                 down_size,
+                 dropout=0.0,
+                 adapter_scalar="learnable_scalar",
+                 adapter_layernorm_option="none"):
         super().__init__()
+        self.n_embd = n_embed
+        self.down_size = down_size
 
-        self.norm = nn.LayerNorm(dim)
+        #_before
+        self.adapter_layernorm_option = adapter_layernorm_option
 
+        self.adapter_layer_norm_before = None
+        if adapter_layernorm_option == "in" or adapter_layernorm_option == "out":
+            self.adapter_layer_norm_before = nn.LayerNorm(self.n_embd)
+
+        if adapter_scalar == "learnable_scalar":
+            self.scale = nn.Parameter(torch.ones(1))
+        else:
+            self.scale = float(adapter_scalar)
+
+        self.down_proj = nn.Linear(self.n_embd, self.down_size)
+        self.non_linear_func = nn.ReLU()
+        self.up_proj = nn.Linear(self.down_size, self.n_embd)
+
+        self.dropout = dropout
+        
+        with torch.no_grad():
+            nn.init.kaiming_uniform_(self.down_proj.weight, a=math.sqrt(5))
+            nn.init.zeros_(self.up_proj.weight)
+            # nn.init.normal_(self.down_proj.weight, std=0.02)
+            # nn.init.normal_(self.up_proj.weight, std=0.02)
+            nn.init.zeros_(self.down_proj.bias)
+            nn.init.zeros_(self.up_proj.bias)
+
+    def forward(self, x):
+        if self.adapter_layernorm_option == 'in':
+            x = self.adapter_layer_norm_before(x)
+
+        down = self.down_proj(x)
+        down = self.non_linear_func(down)
+        down = nn.functional.dropout(down, p=self.dropout, training=self.training)
+        up = self.up_proj(down)
+
+        up = up * self.scale
+
+        if self.adapter_layernorm_option == 'out':
+            up = self.adapter_layer_norm_before(up)
+        output = up
+
+        return output
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4, r=4):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim, eps=1e-6, elementwise_affine=True)
+        self.attn = nn.MultiheadAttention(dim, num_heads, batch_first=True)
+        self.norm2 = nn.LayerNorm(dim)
         self.mlp = nn.Sequential(
             nn.Linear(dim, dim * mlp_ratio),
             nn.GELU(),
             nn.Linear(dim * mlp_ratio, dim),
         )
+        self.adapter = Adapter(n_embed=dim, down_size=dim // r, dropout=0.0, adapter_scalar="1.0", adapter_layernorm_option="in")
 
     def forward(self, x):
-        # The reason for implementing autocast inside forward loop instead
-        # in the main training logic is the implicit forward pass during
-        # memory efficient gradient backpropagation. In backward pass, the
-        # activations need to be recomputed, and if the forward has happened
-        # with mixed precision, the recomputation must also be so. This cannot
-        # be handled with the autocast setup in main training logic.
-        return self.mlp(self.norm(x))
+        residual = x
+        x = self.norm1(x)
+        x, _ = self.attn(x, x, x)
+        residual = residual + x
+        x = self.norm2(residual)
+        out1 = self.mlp(x)
+        out2 = self.adapter(x)
+        return residual + out1 + out2
 
 
-class AttentionSubBlock(nn.Module):
-    """
-    This creates the function F such that the entire block can be
-    expressed as F(G(X)). Includes pre-LayerNorm.
-    """
-    def __init__(
-        self,
-        dim,
-        num_heads
-    ):
+class AttentionBlock(nn.Module):
+    def __init__(self, dim, num_heads, r=4):
         super().__init__()
         self.norm = nn.LayerNorm(dim, eps=1e-6, elementwise_affine=True)
-
-        # using vanilla attention for simplicity. To support adanced attention
-        # module see pyslowfast.
-        # Note that the complexity of the attention module is not a concern
-        # since it is used blackbox as F block in the reversible logic and
-        # can be arbitrary.
         self.attn = nn.MultiheadAttention(dim, num_heads, batch_first=True)
+        self.adapter = Adapter(n_embed=dim, down_size=dim // r, dropout=0.0, adapter_scalar="1.0", adapter_layernorm_option="in")
 
     def forward(self, x):
-        # See MLP fwd pass for explanation.
+        residual = x
         x = self.norm(x)
-        out, _ = self.attn(x, x, x)
-        return out
+        out1, _ = self.attn(x, x, x)
+        out2 = self.adapter(x)
+        return residual + out1 + out2
 
 
-class InvertibleTransformerBlock(invertible_nn.layers.CouplingBlock):
-    def __init__(self, dim, num_heads):
+class MLPBlock(nn.Module):
+    def __init__(self, dim, mlp_ratio=4, r=4):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, dim * mlp_ratio),
+            nn.GELU(),
+            nn.Linear(dim * mlp_ratio, dim),
+        )
+        self.adapter = Adapter(n_embed=dim, down_size=dim//r, dropout=0.0, adapter_scalar="1.0", adapter_layernorm_option="in")
+
+    def forward(self, x):
+        residual = x
+        x = self.norm(x)
+        out1 = self.mlp(x)
+        out2 = self.adapter(x)
+        return residual + out1 + out2
+
+
+class MEFT_Block1(invertible_nn.scaled_coupling_layer.ScaledCouplingBlock):
+    def __init__(self, embed_dim, n_head, reduction_ratio):
+        self.embed_dim = embed_dim
+        self.n_head = n_head
+        self.reduction_ratio = reduction_ratio
+        self.x1_factor = 0.1
+        self.x2_factor = 1.0
+
+        transformer_block = TransformerBlock(dim=self.embed_dim, num_heads=self.n_head, mlp_ratio=4, r=self.reduction_ratio)
+        adapter = MLPBlock(dim=self.embed_dim, mlp_ratio=4, r=self.reduction_ratio)
+
         super().__init__(
-            AttentionSubBlock(dim=dim, num_heads=num_heads),
-            MLPSubblock(dim=dim)
+            F=transformer_block,
+            G=adapter,
+            X1_factor=self.x1_factor,
+            X2_factor=self.x2_factor,
+            switch=True
+        )
+
+
+class MEFT_Block2(invertible_nn.scaled_coupling_layer.ScaledCouplingBlock):
+    def __init__(self, embed_dim, n_head, reduction_ratio):
+        self.embed_dim = embed_dim
+        self.n_head = n_head
+        self.reduction_ratio = reduction_ratio
+        self.x1_factor = 1.0
+        self.x2_factor = 0.1
+
+        transformer_block = TransformerBlock(dim=self.embed_dim, num_heads=self.n_head, mlp_ratio=4, r=self.reduction_ratio)
+        adapter = MLPBlock(dim=self.embed_dim, mlp_ratio=4, r=self.reduction_ratio)
+
+        super().__init__(
+            F=transformer_block,
+            G=adapter,
+            X1_factor=self.x1_factor,
+            X2_factor=self.x2_factor,
+            switch=True
+        )
+
+
+class MEFT_Block3(invertible_nn.scaled_coupling_layer.ScaledCouplingBlock):
+    def __init__(self, embed_dim, n_head, reduction_ratio):
+        self.embed_dim = embed_dim
+        self.n_head = n_head
+        self.reduction_ratio = reduction_ratio
+        self.x1_factor = 0.1
+        self.x2_factor = 0.1
+
+        attention = AttentionBlock(dim=self.embed_dim, num_heads=self.n_head, r=self.reduction_ratio)
+        mlp = MLPBlock(dim=self.embed_dim, mlp_ratio=4, r=self.reduction_ratio)
+
+        super().__init__(
+            F=attention,
+            G=mlp,
+            X1_factor=self.x1_factor,
+            X2_factor=self.x2_factor,
+            switch=True
         )
 
 
@@ -90,7 +199,7 @@ class InvertibleVisionTransformer(nn.Module):
 
         self.layers = nn.ModuleList(
             [
-                InvertibleTransformerBlock(
+                MEFT_Block1(
                     dim=self.embed_dim,
                     num_heads=self.n_head
                 )
@@ -107,12 +216,6 @@ class InvertibleVisionTransformer(nn.Module):
             torch.zeros(1, num_patches, self.embed_dim)
         )
 
-        # The two streams are concatenated and passed through a linear
-        # layer for final projection. This is the only part of RevViT
-        # that uses different parameters/FLOPs than a standard ViT model.
-        # Note that fusion can be done in several ways, including
-        # more expressive methods like in paper, but we use
-        # linear layer + LN for simplicity.
         self.head = nn.Linear(2 * self.embed_dim, num_classes, bias=True)
         self.norm = nn.LayerNorm(2 * self.embed_dim)
 
@@ -120,12 +223,12 @@ class InvertibleVisionTransformer(nn.Module):
         x = self.patch_embed(x).flatten(2).transpose(1, 2)
         x += self.pos_embeddings
 
-        # the two streams X_1 and X_2 are initialized identically with x and
-        # concatenated along the last dimension to pass into the reversible blocks
-        x = torch.cat([x, x], dim=-1)
-
+        x1, x2 = x, x
+        output_hooks = None
         for layer in self.layers:
-            x = layer(x)
+            x1, x2, output_hooks = layer(x1, x2, output_hooks)
+        
+        x = torch.cat([x1, x2], dim=-1)
 
         # aggregate across sequence length
         x = x.mean(1)
